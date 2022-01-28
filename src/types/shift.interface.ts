5481acf4013b0e4a2abc3d1427365de528470fed
liadaltif <liadcraft@gmail.com>
2022-01-28 12:04:18 +0200

    defiend the user and shift model

---
+++ src/types/shift.interface.ts
@@ -0,0 +1,10 @@
+export interface Shift {
+  _id: string;
+  shiftDate: Date;
+  shiftName: string;
+  shiftPerson: string;
+  shiftPersonId: string;
+  startTimeValue: string;
+  endTimeValue: string;
+  traded: boolean;
+}
